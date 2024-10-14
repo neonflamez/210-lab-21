c7620a7d30d782cb13e36f9ad1de0d497fb4d7a1
Shayan Khan <neonclanflamez@gmail.com>
2024-10-13 22:25:39 -0800

    Second Commit

--- main.cpp
+++ main.cpp
@@ -9,3 +9,22 @@ using namespace std;
 const int MIN_NR = 5, MAX_NR = 20;
 const string NAMES[] = {"Senior", "Godlike", "Old", "Mature", "Teen", "Young", "Junior", "Ancient", "Elder", "Master", "Legendary", "Swift", "Nimble", "Brave", "Bold"};
 const string COLORS[] = {"Yellow", "Red", "Gold", "Mauve", "White", "Black", "Blue", "Green", "Silver", "Orange", "Purple", "Brown", "Gray", "Pink", "Turquoise"};
+
+class Goat{
+    public:
+    int age;
+    string name;
+    string color;
+
+    Goat(){
+        age = rand() % 20 + 1;
+        name = NAMES[rand() % 15];
+        color = COLORS[rand() % 15];
+    }
+
+    Goat(int a, string n, string c) : age(a), name(n), color(c) {}
+
+    void display() const {
+        cout << name << " (" << color << " , " << age << ")" << endl;
+    }
+};
