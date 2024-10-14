33f56a637797f03d412360fdddebb3a3e04c76c5
Shayan Khan <neonclanflamez@gmail.com>
2024-10-13 22:14:52 -0800

    First Commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
 // COMSC 210 || Lab 21 || Shayan Khan
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const int MIN_NR = 5, MAX_NR = 20;
+const string NAMES[] = {"Senior", "Godlike", "Old", "Mature", "Teen", "Young", "Junior", "Ancient", "Elder", "Master", "Legendary", "Swift", "Nimble", "Brave", "Bold"};
+const string COLORS[] = {"Yellow", "Red", "Gold", "Mauve", "White", "Black", "Blue", "Green", "Silver", "Orange", "Purple", "Brown", "Gray", "Pink", "Turquoise"};
